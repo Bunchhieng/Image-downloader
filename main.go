8a8bac3300ba8637f3e7052c9574e0d7efc93a7e
Bunchhieng <Bunchhieng@gmail.com>
2016-04-18 13:57:39 -0400

    Download more images

--- main.go
+++ main.go
@@ -10,7 +10,6 @@ import (
 	"os"
 	"io"
 	"strings"
-	"strconv"
 	"fmt"
 	"time"
 )
@@ -22,50 +21,57 @@ type Query struct {
 
 // Right now we only want Title, Image Link and the file extension
 type Item struct {
-	Title string `json:"title"`
-	Link  string `json:"link"`
-	Mime string `json:"mime"`
+	Title       string `json:"title"`
+	Link        string `json:"link"`
+	DisplayLink string `json:"displayLink"`
+	Mime        string `json:"mime"`
 }
 
 func main() {
 	start := time.Now()
 	// Link to the Google custom search API
-	URL := "https://www.googleapis.com/customsearch/v1?cx=001106611627702700888%3Aaonktv-oz_w&q=bells%20palsy%20mouth&exactTerms=palsy&fileType=png&imgColorType=color&imgType=face&searchType=image&key=AIzaSyAYqQ4IxUHnF7rfvzSvnczxQ-u93AbkC8k"
+	// Each request return 10 images.
+	// To get to the next page, we need to change startindex=i + 10
+	URL := "https://www.googleapis.com/customsearch/v1?start=%d&cx=001106611627702700888%%3Aaonktv-oz_w&q=bells%%20palsy%%20mouth&exactTerms=palsy&fileType=png&imgColorType=color&imgType=face&searchType=image&key=AIzaSyAYqQ4IxUHnF7rfvzSvnczxQ-u93AbkC8k"
+	for v := 1; v < 100; v += 10 {
+		val := fmt.Sprintf(URL, v)
+		res, err := http.Get(val)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer res.Body.Close()
+		if res.StatusCode == 200 {
+			decoder := json.NewDecoder(res.Body)
+			var i Query
+			err := decoder.Decode(&i)
+			if err != nil {
+				log.Fatal(err)
+			}
+			dir, err := os.Getwd()
+			for _, i := range i.Items {
+				imgRes := downloadImg(i.Link)
+				os.Mkdir(dir + "/img/", 0777)
+				file, err := os.Create(dir + "/img/" + i.DisplayLink + "." + strings.Split(i.Mime, "/")[1])
+				if err != nil {
+					log.Fatal(err)
+				}
+				_, err = io.Copy(file, imgRes.Body)
+				if err != nil {
+					log.Fatal(err)
+				}
+				file.Close()
+			}
+		}
+	}
+	elapsed := time.Since(start).Seconds()
+	fmt.Printf("Run time: %f secs\n", elapsed)
+	fmt.Println("Sucess!")
+}
 
-	res, err := http.Get(URL)
-	defer res.Body.Close()
+func downloadImg(url string) (res *http.Response) {
+	res, err := http.Get(url)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if res.StatusCode == 200 {
-		decoder := json.NewDecoder(res.Body)
-		var i Query
-		err := decoder.Decode(&i)
-		if err != nil {
-			log.Fatal(err)
-		}
-		j := 0
-		dir, err := os.Getwd()
-		for _, i := range i.Items {
-			res, err := http.Get(i.Link)
-			if err != nil {
-				log.Fatal(err)
-			}
-			os.Mkdir(dir + "/img/", 0777)
-			file, err := os.Create(dir + "/img/" + strconv.Itoa(j) + "." + strings.Split(i.Mime, "/")[1])
-			j++
-			if err != nil {
-				log.Fatal(err)
-			}
-			_, err = io.Copy(file, res.Body)
-			if err != nil {
-				log.Fatal(err)
-			}
-			file.Close()
-			res.Body.Close()
-		}
-		elapsed := time.Since(start).Seconds()
-		fmt.Printf("Run time: %f secs\n", elapsed)
-		fmt.Println("Sucess! Images located at " + dir + "/img/")
-	}
+	return res
 }
